ee447c2c21e272ab6ed3518e05139bd578a94ce1
Rodolfo Alexander Silva Pantoja <rodolfo.silva@globant.com>
2020-06-23 14:15:38 -0500

    Heroku: Some clean code.

--- src/main/java/com/heroku/controllers/SuggestionsController.java
+++ src/main/java/com/heroku/controllers/SuggestionsController.java
@@ -1,7 +1,6 @@
 // Coveo Backend Coding Challenge
 // Design an API endpoint that provides auto-complete suggestions for large cities.
 //
-// Copyright (c) 2017 Samuel Lambert
 //
 // This software may be modified and distributed under the terms
 // of the MIT license. See the LICENSE file for details.
